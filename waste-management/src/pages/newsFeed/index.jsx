fdc1e7f47d1ed4200c708f3bcb07953402cc31a4
MrPhilus <theooophii@gmail.com>
2023-12-08 12:15:18 +0100

    updates to styling

--- waste-management/src/pages/newsFeed/index.jsx
+++ waste-management/src/pages/newsFeed/index.jsx
@@ -2,25 +2,25 @@ import CardSkeleton from "../../components/CardSkeleton";
 import { Link } from "react-router-dom";
 import img from "../../assets/images/coming-soon-bg.jpg";
 import { newsFeed } from "./Data";
+import newsbanner from "../../assets/images/newsbanner.png";
 
 const NewsFeed = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center mb-14">
-        <h1 className="text-3xl">page under construction!</h1>
-        <h2 className="text-xl">stay tuned on!</h2>
+      <div className="flex flex-col items-center justify-center mb-4">
+        <img className="w-1/5" src={newsbanner} alt="news banner" />
+        <h1 className="text-xl font-bold">GET THE LATEST</h1>
       </div>
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          // border: "2px solid red",
           placeItems: "center",
         }}
       >
         {newsFeed.map((feed) => {
           return (
-            <Link key={feed.id} to='/details'>
+            <Link key={feed.id} to="/details">
               <CardSkeleton
                 img={img}
                 title={feed.title}
